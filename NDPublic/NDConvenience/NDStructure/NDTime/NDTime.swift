263e69cea04d9f7651c4acbb5f0890b0a5f33762
JasonChengzi <3923240@qq.com>
2016-11-08 14:23:41 +0800

    update

---
+++ NDPublic/NDConvenience/NDStructure/NDTime/NDTime.swift
@@ -0,0 +1,17 @@
+//
+//  NDTime.swift
+//  NewDesignApplicationDemo
+//
+//  Created by Jason.Chengzi on 11/8/16.
+//  Copyright © 2016 CherryBlossom. All rights reserved.
+//
+
+import Foundation
+
+typealias Time = NDTime
+
+struct NDTime {
+    var hour : Int
+    var minute : Int
+    var second : Int
+}
